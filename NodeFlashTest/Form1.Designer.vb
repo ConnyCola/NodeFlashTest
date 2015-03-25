960b2192bca5137a50b48d94c99161fb9c1115c3
ConnyCola <matthiaswagner@m-mbp.fritz.box>
2015-03-25 22:57:24 +0100

    first push
    Flash and test tool for MSP430

---
+++ NodeFlashTest/Form1.Designer.vb
@@ -0,0 +1,297 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnARD = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ComboBoxARD = New System.Windows.Forms.ComboBox()
+        Me.ComboBoxMSP = New System.Windows.Forms.ComboBox()
+        Me.btnMSP = New System.Windows.Forms.Button()
+        Me.btnTest = New System.Windows.Forms.Button()
+        Me.checkARD = New System.Windows.Forms.CheckBox()
+        Me.checkFLASH = New System.Windows.Forms.CheckBox()
+        Me.checkSERIAL = New System.Windows.Forms.CheckBox()
+        Me.checkVBAT = New System.Windows.Forms.CheckBox()
+        Me.checkVCC = New System.Windows.Forms.CheckBox()
+        Me.checkSD = New System.Windows.Forms.CheckBox()
+        Me.checkRF = New System.Windows.Forms.CheckBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnARD
+        '
+        Me.btnARD.Location = New System.Drawing.Point(215, 101)
+        Me.btnARD.Name = "btnARD"
+        Me.btnARD.Size = New System.Drawing.Size(167, 73)
+        Me.btnARD.TabIndex = 0
+        Me.btnARD.Text = "Arduino connect"
+        Me.btnARD.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.InfoText
+        Me.TextBox1.Font = New System.Drawing.Font("Consolas", 9.047121!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.TextBox1.Location = New System.Drawing.Point(33, 333)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(1196, 517)
+        Me.TextBox1.TabIndex = 1
+        '
+        'ComboBoxARD
+        '
+        Me.ComboBoxARD.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.916231!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBoxARD.FormattingEnabled = True
+        Me.ComboBoxARD.Location = New System.Drawing.Point(215, 45)
+        Me.ComboBoxARD.Name = "ComboBoxARD"
+        Me.ComboBoxARD.Size = New System.Drawing.Size(167, 33)
+        Me.ComboBoxARD.TabIndex = 2
+        '
+        'ComboBoxMSP
+        '
+        Me.ComboBoxMSP.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.916231!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBoxMSP.FormattingEnabled = True
+        Me.ComboBoxMSP.Location = New System.Drawing.Point(26, 45)
+        Me.ComboBoxMSP.Name = "ComboBoxMSP"
+        Me.ComboBoxMSP.Size = New System.Drawing.Size(167, 33)
+        Me.ComboBoxMSP.TabIndex = 3
+        '
+        'btnMSP
+        '
+        Me.btnMSP.Location = New System.Drawing.Point(26, 101)
+        Me.btnMSP.Name = "btnMSP"
+        Me.btnMSP.Size = New System.Drawing.Size(166, 73)
+        Me.btnMSP.TabIndex = 4
+        Me.btnMSP.Text = "MSP connect"
+        Me.btnMSP.UseVisualStyleBackColor = True
+        '
+        'btnTest
+        '
+        Me.btnTest.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.94764!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTest.Location = New System.Drawing.Point(214, 30)
+        Me.btnTest.Name = "btnTest"
+        Me.btnTest.Size = New System.Drawing.Size(196, 84)
+        Me.btnTest.TabIndex = 5
+        Me.btnTest.Text = "Test"
+        Me.btnTest.UseVisualStyleBackColor = True
+        '
+        'checkARD
+        '
+        Me.checkARD.AutoSize = True
+        Me.checkARD.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.checkARD.ForeColor = System.Drawing.Color.Gray
+        Me.checkARD.Location = New System.Drawing.Point(15, 30)
+        Me.checkARD.Name = "checkARD"
+        Me.checkARD.Size = New System.Drawing.Size(170, 30)
+        Me.checkARD.TabIndex = 6
+        Me.checkARD.Text = "Arduino COM"
+        Me.checkARD.UseVisualStyleBackColor = True
+        '
+        'checkFLASH
+        '
+        Me.checkFLASH.AutoSize = True
+        Me.checkFLASH.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.checkFLASH.ForeColor = System.Drawing.Color.Gray
+        Me.checkFLASH.Location = New System.Drawing.Point(15, 137)
+        Me.checkFLASH.Name = "checkFLASH"
+        Me.checkFLASH.Size = New System.Drawing.Size(160, 30)
+        Me.checkFLASH.TabIndex = 6
+        Me.checkFLASH.Text = "HEX flashed"
+        Me.checkFLASH.UseVisualStyleBackColor = True
+        '
+        'checkSERIAL
+        '
+        Me.checkSERIAL.AutoSize = True
+        Me.checkSERIAL.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.checkSERIAL.ForeColor = System.Drawing.Color.Gray
+        Me.checkSERIAL.Location = New System.Drawing.Point(15, 173)
+        Me.checkSERIAL.Name = "checkSERIAL"
+        Me.checkSERIAL.Size = New System.Drawing.Size(143, 30)
+        Me.checkSERIAL.TabIndex = 6
+        Me.checkSERIAL.Text = "MSP COM"
+        Me.checkSERIAL.UseVisualStyleBackColor = True
+        '
+        'checkVBAT
+        '
+        Me.checkVBAT.AutoSize = True
+        Me.checkVBAT.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.checkVBAT.ForeColor = System.Drawing.Color.Gray
+        Me.checkVBAT.Location = New System.Drawing.Point(15, 101)
+        Me.checkVBAT.Name = "checkVBAT"
+        Me.checkVBAT.Size = New System.Drawing.Size(145, 30)
+        Me.checkVBAT.TabIndex = 6
+        Me.checkVBAT.Text = "V_BAT OK"
+        Me.checkVBAT.UseVisualStyleBackColor = True
+        '
+        'checkVCC
+        '
+        Me.checkVCC.AutoSize = True
+        Me.checkVCC.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.checkVCC.ForeColor = System.Drawing.Color.Gray
+        Me.checkVCC.Location = New System.Drawing.Point(15, 66)
+        Me.checkVCC.Name = "checkVCC"
+        Me.checkVCC.Size = New System.Drawing.Size(123, 30)
+        Me.checkVCC.TabIndex = 6
+        Me.checkVCC.Text = "VCC OK"
+        Me.checkVCC.UseVisualStyleBackColor = True
+        '
+        'checkSD
+        '
+        Me.checkSD.AutoSize = True
+        Me.checkSD.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.checkSD.ForeColor = System.Drawing.Color.Gray
+        Me.checkSD.Location = New System.Drawing.Point(15, 210)
+        Me.checkSD.Name = "checkSD"
+        Me.checkSD.Size = New System.Drawing.Size(160, 30)
+        Me.checkSD.TabIndex = 6
+        Me.checkSD.Text = "SD mounted"
+        Me.checkSD.UseVisualStyleBackColor = True
+        '
+        'checkRF
+        '
+        Me.checkRF.AutoSize = True
+        Me.checkRF.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.checkRF.ForeColor = System.Drawing.Color.Gray
+        Me.checkRF.Location = New System.Drawing.Point(15, 246)
+        Me.checkRF.Name = "checkRF"
+        Me.checkRF.Size = New System.Drawing.Size(115, 30)
+        Me.checkRF.TabIndex = 6
+        Me.checkRF.Text = "RF com"
+        Me.checkRF.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(36, 304)
+        Me.ProgressBar1.MarqueeAnimationSpeed = 40
+        Me.ProgressBar1.Maximum = 70
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(1195, 23)
+        Me.ProgressBar1.TabIndex = 7
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.checkARD)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.checkFLASH)
+        Me.GroupBox1.Controls.Add(Me.checkRF)
+        Me.GroupBox1.Controls.Add(Me.checkSERIAL)
+        Me.GroupBox1.Controls.Add(Me.btnTest)
+        Me.GroupBox1.Controls.Add(Me.checkSD)
+        Me.GroupBox1.Controls.Add(Me.checkVBAT)
+        Me.GroupBox1.Controls.Add(Me.checkVCC)
+        Me.GroupBox1.Location = New System.Drawing.Point(36, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(427, 286)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Node test"
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.12566!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(214, 127)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(196, 149)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "pass"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.ComboBoxMSP)
+        Me.GroupBox2.Controls.Add(Me.btnARD)
+        Me.GroupBox2.Controls.Add(Me.btnMSP)
+        Me.GroupBox2.Controls.Add(Me.ComboBoxARD)
+        Me.GroupBox2.Location = New System.Drawing.Point(825, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(406, 286)
+        Me.GroupBox2.TabIndex = 10
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Serial connection"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(517, 42)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(214, 102)
+        Me.Button1.TabIndex = 11
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1257, 868)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.Text = "NodeFlashTest"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnARD As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBoxARD As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBoxMSP As System.Windows.Forms.ComboBox
+    Friend WithEvents btnMSP As System.Windows.Forms.Button
+    Friend WithEvents btnTest As System.Windows.Forms.Button
+    Friend WithEvents checkARD As System.Windows.Forms.CheckBox
+    Friend WithEvents checkFLASH As System.Windows.Forms.CheckBox
+    Friend WithEvents checkSERIAL As System.Windows.Forms.CheckBox
+    Friend WithEvents checkVBAT As System.Windows.Forms.CheckBox
+    Friend WithEvents checkVCC As System.Windows.Forms.CheckBox
+    Friend WithEvents checkSD As System.Windows.Forms.CheckBox
+    Friend WithEvents checkRF As System.Windows.Forms.CheckBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ NodeFlashTest/Form1.vb
@@ -0,0 +1,362 @@
+﻿Imports System.IO.Ports
+Imports System.Threading
+
+
+
+Public Class Form1
+    Private WithEvents ComPortARD As IO.Ports.SerialPort
+    Private WithEvents ComPortMSP As IO.Ports.SerialPort
+
+
+    Dim cmdFlash1 As String = "/ti/MSP430Flasher_1.3.1/MSP430Flasher.exe -n CC430F6137 -w " & Chr(34) & "/ti/MSP430Flasher_1.3.1/Firmware.txt" & Chr(34) & " -v -z [VCC] -m SBW2 (-i "
+    Dim cmdFlash2 As String = ") (-e ERASE_ALL)"
+
+    Private EndOfCommand As String = Constants.vbCr.ToString
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Label1.Visible = False
+        Label1.BackColor = Color.LightGreen
+
+        ComboBoxARD.Items.AddRange(IO.Ports.SerialPort.GetPortNames)
+        ComboBoxMSP.Items.AddRange(IO.Ports.SerialPort.GetPortNames)
+
+
+        If ComboBoxARD.Items.Count > 0 Then
+            ComboBoxARD.SelectedIndex = 0
+            ComboBoxMSP.SelectedIndex = 0
+        End If
+        If ComboBoxARD.Items.Count > 1 Then
+            ComboBoxARD.SelectedIndex = 0
+            ComboBoxMSP.SelectedIndex = 1
+        End If
+
+
+    End Sub
+
+    Private Sub btnMSP_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnMSP.Click
+        Try
+            ComPortMSP.Close()
+        Catch ex As Exception
+        End Try
+        Static Dim MSPop = 0
+
+        ComPortMSP = New IO.Ports.SerialPort(ComboBoxMSP.SelectedItem.ToString)
+        ComPortMSP.BaudRate = 9600
+        ComPortMSP.DataBits = 8
+        ComPortMSP.StopBits = IO.Ports.StopBits.One
+        ComPortMSP.Parity = IO.Ports.Parity.None
+        ComPortMSP.ReceivedBytesThreshold = 1
+
+
+        If MSPop = 0 Then
+            ComPortMSP.Open()
+
+            If ComPortMSP.IsOpen Then
+                MSPop = 1
+            Else
+                MSPop = 0
+            End If
+        Else
+            ComPortMSP.Close()
+            If Not ComPortMSP.IsOpen Then
+                MSPop = 0
+            Else
+                MSPop = 1
+            End If
+        End If
+
+        If MSPop Then
+            btnMSP.BackColor = Color.LightGreen
+            checkSERIAL.Checked = True
+            checkSERIAL.ForeColor = Color.Green
+        Else
+            btnMSP.BackColor = Color.LightGray
+        End If
+    End Sub
+
+    Private Sub btnARD_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnARD.Click
+        Try
+            ComPortARD.Close()
+            Thread.Sleep(500)
+        Catch ex As Exception
+        End Try
+
+        Static Dim ARDop = 0
+
+        ComPortARD = New IO.Ports.SerialPort(ComboBoxARD.SelectedItem.ToString)
+        ComPortARD.BaudRate = 9600
+        ComPortARD.DataBits = 8
+        ComPortARD.StopBits = IO.Ports.StopBits.One
+        ComPortARD.Parity = IO.Ports.Parity.None
+        ComPortARD.ReceivedBytesThreshold = 1
+
+
+        If ARDop = 0 Then
+            ComPortARD.Open()
+
+            If ComPortARD.IsOpen Then
+                ARDop = 1
+            Else
+                ARDop = 0
+            End If
+        Else
+            ComPortARD.Close()
+            Thread.Sleep(500)
+            If Not ComPortARD.IsOpen Then
+                ARDop = 0
+            Else
+                ARDop = 1
+            End If
+        End If
+
+        If ARDop Then
+            btnARD.BackColor = Color.LightGreen
+            checkARD.ForeColor = Color.Green
+            checkARD.Checked = True
+        Else
+            btnARD.BackColor = Color.LightGray
+        End If
+
+
+    End Sub
+
+    Private Sub serialARD_event(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComPortARD.DataReceived
+        'Try
+        '    ARD_addText(ComPortARD.ReadLine)
+        'Catch ex As Exception
+
+        'End Try
+
+
+    End Sub
+
+    Private Delegate Sub ARD_Delegate(ByVal message As String, ByVal addDateTime As Boolean)
+    Private Sub ARD_addText(ByVal message As String, Optional ByVal addDateTime As Boolean = False)
+
+        With TextBox1
+            If .InvokeRequired Then
+                .Invoke(New LogDelegate(AddressOf ARD_addText), message, addDateTime)
+            Else
+                .AppendText(message & vbNewLine)
+            End If
+        End With
+
+    End Sub
+
+    Private Sub serialMSP_event(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComPortMSP.DataReceived
+        MSP_addText(ComPortMSP.ReadLine)
+    End Sub
+
+    Private Delegate Sub MSP_Delegate(ByVal message As String, ByVal addDateTime As Boolean)
+    Private Sub MSP_addText(ByVal message As String, Optional ByVal addDateTime As Boolean = False)
+
+        With TextBox1
+            If .InvokeRequired Then
+                .Invoke(New LogDelegate(AddressOf MSP_addText), message, addDateTime)
+            Else
+                .AppendText(message & vbNewLine)
+            End If
+        End With
+        If message Is Nothing Then
+        ElseIf message.Contains("Open File  INI.txt             rc= 0") Then
+            checkSD.Checked = True
+            checkSD.ForeColor = Color.Green
+        ElseIf message.Contains("Open File  nwk.txt             rc= 3") Then
+            checkRF.Checked = True
+            checkRF.ForeColor = Color.Green
+        End If
+    End Sub
+
+
+    Private Sub btnTest_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnTest.Click
+
+        ProgressBar1.Value = 0
+        Label1.Visible = False
+        Label1.Text = ""
+        Label1.Update()
+        TextBox1.Text = ""
+
+        checkARD.ForeColor = Color.Gray
+        checkARD.Checked = False
+        checkSD.ForeColor = Color.Gray
+        checkSD.Checked = False
+        checkSERIAL.ForeColor = Color.Gray
+        checkSERIAL.Checked = False
+        checkRF.ForeColor = Color.Gray
+        checkRF.Checked = False
+        checkFLASH.ForeColor = Color.Gray
+        checkFLASH.Checked = False
+        checkVBAT.ForeColor = Color.Gray
+        checkVBAT.Checked = False
+        checkVCC.ForeColor = Color.Gray
+        checkVCC.Checked = False
+
+        'Connect Arduino
+        Try
+            If ComPortARD.IsOpen Then
+                checkARD.ForeColor = Color.Green
+                checkARD.Checked = True
+            End If
+        Catch ex As Exception
+            btnARD_Click(Nothing, Nothing)
+            Thread.Sleep(1000)
+        End Try
+
+
+        sendArduino("2")         'Turn VBat ON
+        sendArduino("0")          'Turn Vin ON
+        Dim vbat = receiveArduino("5")
+        If vbat >= 0 And vbat < 999 Then       'Read Vbat
+            checkVBAT.Checked = True
+            checkVBAT.ForeColor = Color.Green
+        End If
+
+        sendArduino("2")         'Turn VBat ON
+        sendArduino("1")         'Turn Vin OFF
+        Dim vcc = receiveArduino("4")
+        If vcc >= 0 And vcc < 999 Then       'Read Vcc
+            checkVCC.Checked = True
+            checkVCC.ForeColor = Color.Green
+        End If
+
+        'Connect MSP430
+
+        Try
+            If ComPortMSP.IsOpen Then
+                btnMSP_Click(Nothing, Nothing)
+                Thread.Sleep(500)
+            End If
+        Catch ex As Exception
+        End Try
+
+
+        Using p As New Process
+            AddHandler p.OutputDataReceived, AddressOf Me.Process_OutputDataReceived
+            ' Process definieren
+            With p.StartInfo
+                .FileName = "cmd"
+                .CreateNoWindow = True
+                .UseShellExecute = False
+                .RedirectStandardInput = True
+                .RedirectStandardOutput = True
+            End With
+            ' Process starten
+            p.Start()
+            ' Auszuführende Befehle
+            Using sw As IO.StreamWriter = p.StandardInput
+                sw.AutoFlush = True
+                sw.WriteLine("cd /")
+                sw.WriteLine("ping www.vb-paradise.de")
+                'sw.WriteLine(cmdFlash1 & ComboBoxMSP.SelectedItem.ToString & cmdFlash2)
+
+            End Using
+            ' Ausgabe
+            p.BeginOutputReadLine()
+
+        End Using
+
+    End Sub
+
+    Private Delegate Sub LogDelegate(ByVal message As String, ByVal addDateTime As Boolean)
+    Private Sub AppendLogLine(ByVal message As String, Optional ByVal addDateTime As Boolean = False)
+        With TextBox1
+            If .InvokeRequired Then
+                .Invoke(New LogDelegate(AddressOf Me.AppendLogLine), message, addDateTime)
+            Else
+                .AppendText(message & vbNewLine)
+            End If
+        End With
+        If message Is Nothing Then
+        Else
+            If message.Contains("* Driver      : closed (No error)") Then
+                checkFLASH.ForeColor = Color.Green
+                checkFLASH.Checked = True
+            End If
+        End If
+    End Sub
+
+    Private Sub Process_OutputDataReceived(ByVal sender As Object, ByVal e As System.Diagnostics.DataReceivedEventArgs)
+        Me.AppendLogLine(e.Data, False)
+    End Sub
+
+
+    Private Sub checkFLASH_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles checkFLASH.CheckedChanged
+        'next step
+        checkFLASH.Update()
+        If checkFLASH.Checked = True Then
+            ProgressBar1.Value += 10
+            ProgressBar1.Update()
+            Thread.Sleep(2000)
+            btnMSP_Click(Nothing, Nothing)
+        End If
+    End Sub
+
+
+
+    Private Sub checkARD_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles checkARD.CheckedChanged
+        If checkARD.Checked = True Then
+            ProgressBar1.Value += 10
+            ProgressBar1.Update()
+        End If
+    End Sub
+
+    Private Sub checkVBAT_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles checkVBAT.CheckedChanged
+        If checkVBAT.Checked = True Then
+            ProgressBar1.Value += 10
+            ProgressBar1.Update()
+        End If
+    End Sub
+
+    Private Sub checkVCC_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles checkVCC.CheckedChanged
+        If checkVCC.Checked = True Then
+            ProgressBar1.Value += 10
+            ProgressBar1.Update()
+        End If
+    End Sub
+
+    Private Sub checkSERIAL_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles checkSERIAL.CheckedChanged
+        If checkSERIAL.Checked = True Then
+            ProgressBar1.Value += 10
+            ProgressBar1.Update()
+        End If
+    End Sub
+
+    Private Sub checkSD_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles checkSD.CheckedChanged
+        If checkSD.Checked = True Then
+            ProgressBar1.Value += 10
+            ProgressBar1.Update()
+        End If
+    End Sub
+
+    Private Sub checkRF_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles checkRF.CheckedChanged
+        If checkRF.Checked = True Then
+            ProgressBar1.Value += 10
+            ProgressBar1.Update()
+
+            If ProgressBar1.Value = 70 Then
+                Label1.BackColor = Color.LightGreen
+                Label1.Text = "pass"
+            Else
+                Label1.BackColor = Color.Red
+                Label1.Text = " fail!"
+            End If
+            Label1.Visible = True
+        End If
+    End Sub
+
+    Private Sub sendArduino(ByVal command As String)
+        ComPortARD.Write(command & vbCr)
+        Thread.Sleep(100)
+    End Sub
+
+    Private Function receiveArduino(ByVal command As String)
+        ComPortARD.Write(command & vbCr)
+        While ComPortARD.BytesToRead = 0
+        End While
+        Dim inByte As Byte = ComPortARD.ReadByte
+        Return inByte
+    End Function
+
+End Class
